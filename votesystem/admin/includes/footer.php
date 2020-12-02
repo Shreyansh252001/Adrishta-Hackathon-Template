c65c38360d5505adea413ee3d1952e64661e8d09
shreyansh252001 <shreyansh252001@gmail.com>
2020-12-02 11:23:33 +0530

    one more Last commit

---
+++ votesystem/admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Voting System</b>
+    </div>
+    <strong> &copy; 2020 Brought To You By <a href="https://www.linkedin.com/in/shreyansh-shukla-187259181/">Shreyansh Shukla</a></strong>
+</footer>
